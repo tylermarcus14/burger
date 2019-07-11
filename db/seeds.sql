dcac66c13b48a0b6d032e05723089e5ae359ae0a
tyler <tylermarcus14@gmail.com>
2019-07-11 13:02:34 -0400

    updated files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegan', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper', false);
